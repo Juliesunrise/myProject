662ece5476b8e200540804e13d761769462966c5
Juliesunrise <98773772+Juliesunrise@users.noreply.github.com>
2024-04-27 14:21:43 +0300

    edited files

---
+++ script/app.js
@@ -0,0 +1,24 @@
+//-----------------------------//
+
+let modal=document.querySelector('.modal')
+let btn=document.querySelector('#btn-circle')
+let close=document.querySelector('#btn_close')
+
+if( window.innerWidth <= 768)
+{
+    btn.addEventListener('click', function () {
+
+
+        if (btn) {
+            modal.classList.remove('hide')
+        }
+    })
+
+    close.addEventListener('click', function () {
+        if (close) {
+            modal.classList.add('hide')
+
+        }
+    })
+};
+
